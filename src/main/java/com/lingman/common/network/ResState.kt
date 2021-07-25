06e05d0f0387f63dcc73e756b28b78bf42952624
jackyzhong <jackyecnu@126.com>
2021-07-25 21:41:24 +0800

    1

--- src/main/java/com/lingman/common/network/ResState.kt
+++
@@ -1,8 +0,0 @@
-package com.lingman.common.network
-
-import java.lang.Exception
-
-sealed class ResState<out T : Any> {
-    data class Success<out T : Any>(val data: T) : ResState<T>()
-    data class Error(val exception: Exception) : ResState<Nothing>()
-}
--- src/main/java/com/lingman/common/network/resultObj.kt
+++
@@ -1,8 +0,0 @@
-package com.lingman.taohupai.ui.common
-
-data class ApiResultObj<T> (
-    val code:Int,
-    val data:T,
-    val message:String
-)
-
